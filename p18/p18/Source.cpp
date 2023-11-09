72b0c160eadebb6688398c9618f42ba5119d0b90
Sandyts <st1005127@gmail.com>
2023-11-10 00:43:30 +0800

    m

---
+++ p18/p18/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+
+
+int main() {
+	char s1[60] = "welcome";
+	char s2[60];
+	int len;
+
+	strcpy_s(s2, s1);
+	printf("string2=%s\n", s2);
+
+	len = strlen(s2);
+	printf("length of string2= %d\n", len);
+
+
+
+}
